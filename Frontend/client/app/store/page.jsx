fef7ef937723090e076987af5302f5582328a00c
binhvo292003 <95414345+binhvo292003@users.noreply.github.com>
2024-06-29 21:13:58 +0700

    ui: add maps

--- Frontend/client/app/store/page.jsx
+++ Frontend/client/app/store/page.jsx
@@ -1,8 +1,19 @@
+import classes from "./page.module.css";
+
 export default function StorePage() {
     return (
-        <div>
-            <h1>Store</h1>
-            <p>Store page content</p>
+        <div className={classes["container"]}>
+            <div className={classes['title']}>Store</div>
+            <iframe
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d125428.21695065877!2d106.5646932598018!3d10.762835600000006!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31752f1c06f4e1dd%3A0x43900f1d4539a3d!2zVHLGsOG7nW5nIMSQ4bqhaSBo4buNYyBLaG9hIGjhu41jIFThu7Egbmhpw6puIC0gxJDhuqFpIGjhu41jIFF14buRYyBnaWEgVFAuSENN!5e0!3m2!1svi!2s!4v1719667944880!5m2!1svi!2s"
+                width="600"
+                height="450"
+                style={{ border: 0 }}
+                allowFullScreen=""
+                loading="lazy"
+                referrerPolicy="no-referrer-when-downgrade"
+                className={classes["maps"]}
+            ></iframe>
         </div>
     );
 }
