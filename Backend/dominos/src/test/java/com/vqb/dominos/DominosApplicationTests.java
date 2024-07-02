0f8b100a30b9c689ff0f0e1b059201f94bbbf691
binhvo292003 <95414345+binhvo292003@users.noreply.github.com>
2024-07-02 11:08:08 +0700

    init: initial Spring Boot

---
+++ Backend/dominos/src/test/java/com/vqb/dominos/DominosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vqb.dominos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DominosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
